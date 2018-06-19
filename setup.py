aa342f64da5fccfcb49f1c3bf1f980b77dbb7056
William Schwartz <wkschwartz@gmail.com>
2018-06-18 20:47:15 -0500

    Add minimal setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+	name='shutdown',
+	version='0.1.0',
+	packages=['shutdown'],
+	author='William Schwartz',
+	url='https://github.com/wkschwartz/shutdown',
+	python_requires='>=3.5',
+)
