f416f61fb7242fdcd91c2ccfa14f1734b1578a54
William Schwartz <wkschwartz@gmail.com>
2018-07-25 14:30:36 -0500

    Make shutdown PEP 561-compatible
    This allows mypy to type-check code depending on shutdown without doing
    anything special. See
    http://mypy.readthedocs.io/en/latest/installed_packages.html#installed-packages

--- setup.py
+++ setup.py
@@ -5,7 +5,9 @@ setup(
 	name='shutdown',
 	version='0.1.0',
 	packages=['shutdown'],
+	package_data={'shutdown': ['py.typed']},
 	author='William Schwartz',
 	url='https://github.com/wkschwartz/shutdown',
 	python_requires='>=3.5',
+	zip_safe=False,
 )
