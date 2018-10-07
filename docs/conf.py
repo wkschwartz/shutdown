a88622765e540b8f2386afb1d5a2ee56dfc4713b
William Schwartz <wkschwartz@gmail.com>
2018-10-06 21:56:28 -0500

    Switch from spaces to tabs

--- docs/conf.py
+++ docs/conf.py
@@ -39,10 +39,10 @@ release = '0.2.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
+	'sphinx.ext.autodoc',
+	'sphinx.ext.intersphinx',
+	'sphinx.ext.viewcode',
+	'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -111,29 +111,29 @@ htmlhelp_basename = 'Shutdowndoc'
 # -- Options for LaTeX output ------------------------------------------------
 
 latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
+	# The paper size ('letterpaper' or 'a4paper').
+	#
+	# 'papersize': 'letterpaper',
 
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
+	# The font size ('10pt', '11pt' or '12pt').
+	#
+	# 'pointsize': '10pt',
 
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
+	# Additional stuff for the LaTeX preamble.
+	#
+	# 'preamble': '',
 
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
+	# Latex figure (float) alignment
+	#
+	# 'figure_align': 'htbp',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'Shutdown.tex', 'Shutdown Documentation',
-     'William Schwartz', 'manual'),
+	(master_doc, 'Shutdown.tex', 'Shutdown Documentation',
+	'William Schwartz', 'manual'),
 ]
 
 
@@ -142,8 +142,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'shutdown', 'Shutdown Documentation',
-     [author], 1)
+	(master_doc, 'shutdown', 'Shutdown Documentation',
+	[author], 1)
 ]
 
 
@@ -153,9 +153,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'Shutdown', 'Shutdown Documentation',
-     author, 'Shutdown', 'One line description of project.',
-     'Miscellaneous'),
+	(master_doc, 'Shutdown', 'Shutdown Documentation',
+	author, 'Shutdown', 'One line description of project.',
+	'Miscellaneous'),
 ]
 
 
--- docs/index.rst
+++ docs/index.rst
@@ -1,14 +1,14 @@
 .. Shutdown documentation master file, created by
-   sphinx-quickstart on Sat Oct  6 21:52:56 2018.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
+	sphinx-quickstart on Sat Oct  6 21:52:56 2018.
+	You can adapt this file completely to your liking, but it should at least
+	contain the root `toctree` directive.
 
 Welcome to Shutdown's documentation!
 ====================================
 
 .. toctree::
-   :maxdepth: 2
-   :caption: Contents:
+	:maxdepth: 2
+	:caption: Contents:
 
 
 
