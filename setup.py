a96d867bc0a1632f9622385e559cd58072204c35
William Schwartz <wkschwartz@gmail.com>
2018-10-06 21:20:10 -0500

    Version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
 	name='shutdown',
-	version='0.1.0',
+	version='0.2.0',
 	packages=['shutdown'],
 	package_data={'shutdown': ['py.typed']},
 	author='William Schwartz',
