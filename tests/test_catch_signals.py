03ee9ac920504a6818b3a300722d3e051d10c2e2
William Schwartz <wkschwartz@gmail.com>
2018-10-15 23:35:35 -0500

    Add test of reentrancy of catch_signals (refs #14)

--- tests/test_catch_signals.py
+++ tests/test_catch_signals.py
@@ -249,6 +249,24 @@ class TestCatchSignals(unittest.TestCase):
 			self.assertTrue(self.handler_called)
 		self.assert_logging(logcm.output)
 
+	def test_handler_restored_after_nested_catch_signals(self):
+		self.assertEqual(signal.getsignal(SIG2), signal.SIG_DFL)
+		with self.assertLogs('wrapitup') as logcm, self.catch_signals():
+			signal.signal(SIG2, self.handler)
+			self.assertFalse(requested())
+			with self.catch_signals():
+				self.assertFalse(requested())
+				self.suicide(KILL1)
+				self.assertTrue(requested())
+
+				self.assertFalse(self.handler_called)
+				self.suicide(KILL1)
+				self.assertTrue(self.handler_called)
+			self.assertFalse(requested())
+			self.assertEqual(signal.getsignal(SIG2), self.handler)
+		self.assertFalse(requested())
+		self.assertEqual(signal.getsignal(SIG2), signal.SIG_DFL)
+
 	def test_handler_reset_is_idempotent(self):
 		self.assertFalse(requested())
 		with self.assertLogs('wrapitup') as logcm, self.catch_signals():
